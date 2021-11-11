dc89b53aa84b0280de142d02506f058f41e7c16e
MandyZ1998 <zmdzmd1818@gmail.com>
2021-11-11 16:50:56 -0500

    add test

--- tests/test_cipher_mz2884.py
+++ tests/test_cipher_mz2884.py
@@ -1,1 +1,7 @@
 from cipher_mz2884 import cipher_mz2884
+
+def test_cipher_encrypt(): 
+    example = ['test', 4]
+    expected = 'xiwx'
+    actual = cipher_mz2884.cipher(example[0],example[1])
+    assert actual == expected
